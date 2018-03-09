f48d2107b345c6d6a627d62d995a2763427ad620
Jacek <jacek@ivolution.pl>
2018-03-09 11:18:39 +0100

    learning in progress. Arrow functions, objects decomposition and more.

--- samples/hello_world.js
+++ samples/hello_world.js
@@ -1,4 +1,57 @@
 "use strict";
 var text = "Ala";
-console.log("hello", text);
+var sayHello = function (message) {
+    console.log(message);
+};
+// very short version
+var sayHelloShort = function (message) { console.log(message); };
+sayHello(text);
+sayHelloShort(text);
+//another arrow function thing
+var Person = /** @class */ (function () {
+    function Person(name) {
+        var _this = this;
+        this.sayHi = function () {
+            var _this = this;
+            console.log("not timeoput ", this.name);
+            setTimeout(function () {
+                console.log("Hello, my name is " + _this.name);
+            }, 1000);
+        };
+        this.sayHiArrow = function () {
+            console.log("not timeoput ", _this.name);
+            setTimeout(function () {
+                console.log("Hello Arrow,\n\n\n\n\n              my name is " + _this.name);
+            }, 1000);
+        };
+        this.name = name;
+    }
+    ;
+    return Person;
+}());
+var person = new Person("Blob");
+person.sayHi();
+person.sayHiArrow();
+// object destructuring
+function buildPersonData(_a, address) {
+    var firstName = _a.firstName, lastName = _a.lastName;
+    if (address === void 0) { address = [""]; }
+    return firstName + " " + lastName + " " + address;
+}
+var personData = {
+    firstName: "Kobe",
+    lastName: "Bryant",
+    address: "Staples Center"
+};
+buildPersonData(personData);
+var firstName = "Kobe", lastName = "Bryant";
+var partialAddress = ["Street Name", "Street number", "Po box"];
+var personData2 = {
+    firstName: firstName,
+    lastName: lastName
+};
+var fullAddress = partialAddress.concat(["Staples Center"]);
+buildPersonData(personData2, fullAddress);
+var fullAddress2 = partialAddress.concat(["Staples Center"]);
+buildPersonData(personData2, fullAddress2);
 //# sourceMappingURL=hello_world.js.map
--- samples/hello_world.ts
+++ samples/hello_world.ts
@@ -1,4 +1,84 @@
 let text = "Ala"
-console.log("hello", text);
 
-message = 0;
+
+const sayHello = (message:string) => {
+    console.log(message);
+}
+
+// very short version
+const sayHelloShort = (message:string) => {console.log(message);};
+
+sayHello(text);
+
+sayHelloShort(text);
+
+//another arrow function thing
+
+class Person{
+    name: string;
+    constructor(name:string) {
+        this.name = name;
+    };
+
+    sayHi = function(this: any){  // very weird declaration of this here.. But also similar to Python
+        console.log("not timeoput ", this.name);
+        setTimeout(() => {
+            console.log(`Hello, my name is ${this.name}`);
+        }, 1000);
+    };
+
+    sayHiArrow = () => {  // compared with the above doesn't complain about "this" being any
+        console.log("not timeoput ", this.name);
+        setTimeout(() => {
+            console.log(`Hello Arrow,
+
+
+
+
+              my name is ${this.name}`);
+        }, 1000);
+    };
+
+    // main takeaway:
+    // creating function with => does not create new scope for variables, so sayHiArrow uses Person scope
+    // therefore this is already defined. For regular sayHi this has to be passed nicely as parameter.
+
+}
+
+const person = new Person("Blob");
+
+person.sayHi();
+person.sayHiArrow()
+
+// object destructuring
+
+function buildPersonData({firstName, lastName}, address = [""]){
+  return firstName + " " + lastName +" "+ address; 
+}
+
+const personData = {
+  firstName: "Kobe",
+  lastName: "Bryant",
+  address: "Staples Center"
+};
+
+buildPersonData(personData);
+
+const firstName= "Kobe",
+  lastName= "Bryant";
+
+const partialAddress = ["Street Name", "Street number", "Po box"];
+
+const personData2 = {
+  firstName,
+  lastName
+};
+
+
+const fullAddress = partialAddress.concat(["Staples Center"]);
+
+buildPersonData(personData2, fullAddress);
+
+const fullAddress2 = [...partialAddress, "Staples Center"];
+
+buildPersonData(personData2, fullAddress2);
