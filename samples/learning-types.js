5aa725e00edd13bea16f918b028f1675d203a9af
Jacek <jacek@ivolution.pl>
2018-03-09 21:57:39 +0100

    tuples and arrays and type annotations.

--- samples/learning-types.js
+++ samples/learning-types.js
@@ -39,4 +39,48 @@ var personWithAddress = {
     },
 };
 console.log(personWithAddress);
+// in typescript and in js functions are valid variables
+function createHelloMessage(name) {
+    return "Hello, my name  is " + name;
+}
+var hello = createHelloMessage;
+console.log(hello("Jacek"));
+console.log(hello({ firstName: "Jacek" })); // thats bad.
+function safeCreateHelloMessage(name, extra) {
+    // this function will not accept object as name
+    return "Hello, my name  is " + name;
+}
+// console.log(safeCreateHelloMessage({firstName:"Jacek"})); // fails, due to wrong type.
+// actually fails means that the program will compile and run (sic!) however compiler will error out.
+var message1 = safeCreateHelloMessage("Bill");
+console.log(message1);
+var creator = safeCreateHelloMessage;
+var message3 = creator("Bill");
+console.log(message3);
+// using the interface
+var creatorFromInterface = safeCreateHelloMessage;
+var message4 = creator("Bill 4");
+console.log(message4);
+// arrays strong typing and tuples
+var persons = ["Kobe", "John", "Shaq"];
+var counters = [1, 2, 3];
+//won't work 
+//counters = persons;
+var tuple = ["string", 1];
+var tuple1 = ["name", 1];
+var tuple3 = ["ala", "ma", "kota"];
+// enums
+// 0 - Guard, 1 - Forward, 2 - Center
+// using player from previous example
+var PlayerPosition;
+(function (PlayerPosition) {
+    PlayerPosition[PlayerPosition["Guard"] = 0] = "Guard";
+    PlayerPosition[PlayerPosition["Forward"] = 1] = "Forward";
+    PlayerPosition[PlayerPosition["Center"] = 2] = "Center";
+})(PlayerPosition || (PlayerPosition = {}));
+var kobe = ["Kobe", PlayerPosition["Guard"]]; //option 1.
+var james = ["James", PlayerPosition.Forward]; //option 2.
+var shaq = ["Shaq", PlayerPosition.Center];
+var players = [kobe, james, shaq];
+console.log(players);
 //# sourceMappingURL=learning-types.js.map
--- samples/learning-types.ts
+++ samples/learning-types.ts
@@ -80,3 +80,89 @@ let personWithAddress:PersonWithAddress = {
 }
 
 console.log(personWithAddress);
+
+// in typescript and in js functions are valid variables
+
+function createHelloMessage(name){
+    return `Hello, my name  is ${name}`;
+}
+
+let hello = createHelloMessage;
+console.log(hello("Jacek"));
+
+console.log(hello({firstName:"Jacek"})); // thats bad.
+
+
+function safeCreateHelloMessage(name:string, extra?: number) :string{  //last "string" defines output type
+    // this function will not accept object as name
+    return `Hello, my name  is ${name}`;
+}
+
+// console.log(safeCreateHelloMessage({firstName:"Jacek"})); // fails, due to wrong type.
+// actually fails means that the program will compile and run (sic!) however compiler will error out.
+
+const message1 = safeCreateHelloMessage("Bill");
+console.log(message1);
+
+type MessageCreator = (message:string) => string;  //function type. Accepts string and outputs string;
+
+const creator: MessageCreator = safeCreateHelloMessage;
+
+const message3 = creator("Bill");
+
+console.log(message3);
+
+// there is also an option to create type as interface with one method (mentioned also before);
+interface MessageCreatorInterface{
+    (name:string) :string
+}
+
+// using the interface
+
+const creatorFromInterface: MessageCreatorInterface = safeCreateHelloMessage;
+
+const message4 = creator("Bill 4");
+
+console.log(message4);
+
+// arrays strong typing and tuples
+
+let persons:string[] = ["Kobe", "John", "Shaq"];
+let counters:number[] = [1, 2, 3];
+//won't work 
+//counters = persons;
+
+let tuple = ["string", 1];
+
+type PlayerTuple = [string, number];
+
+let tuple1: PlayerTuple = ["name", 1];
+
+type ThreeNames = [string, string, string];
+
+let tuple3: ThreeNames =["ala", "ma", "kota"];
+    
+// enums
+
+// 0 - Guard, 1 - Forward, 2 - Center
+
+// using player from previous example
+
+
+
+enum PlayerPosition {
+    Guard,
+    Forward,
+    Center, 
+}
+
+let kobe: PlayerTuple = ["Kobe", PlayerPosition["Guard"]];  //option 1.
+let james: PlayerTuple = ["James", PlayerPosition.Forward]; //option 2.
+let shaq: PlayerTuple = ["Shaq", PlayerPosition.Center];
+
+let players:PlayerTuple[] = [kobe, james, shaq];
+console.log(players);
+
+
+
+
