bf95dbdc7a7aafbb8d440bb050db9c78c9b379c3
Jacek <jacek@ivolution.pl>
2018-03-11 17:09:57 +0100

    fixing bugs related to implicitAny

--- samples/buildPersonData.js
+++ samples/buildPersonData.js
@@ -1,5 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// how to avoid any on firstname/lastname dict?
+// create a type with these data, ie persomType and use to replace the dict in question.
+// type PersonType = {
+//     firstName: string,
+//     lastName: string
+// }
+// so the function signature will look like: 
+// function buildPersonData2({firstName, lastName}:PersonType, ...address:any[]){
 function buildPersonData2(_a) {
     var firstName = _a.firstName, lastName = _a.lastName;
     var address = [];
--- samples/buildPersonData.ts
+++ samples/buildPersonData.ts
@@ -1,4 +1,12 @@
-export function buildPersonData2({firstName, lastName}, ...address){
+// how to avoid any on firstname/lastname dict?
+// create a type with these data, ie persomType and use to replace the dict in question.
+// type PersonType = {
+//     firstName: string,
+//     lastName: string
+// }
+// so the function signature will look like: 
+// function buildPersonData2({firstName, lastName}:PersonType, ...address:any[]){
+export function buildPersonData2({firstName, lastName}:any, ...address:any[]){
   // ellipsis here works as **kwargs in python
   return firstName + " " + lastName +" "+ address; 
 } 
