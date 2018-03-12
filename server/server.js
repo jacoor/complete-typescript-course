d23f1a037ef0ab8336d906caab8eb9116776c5a1
Jacek <jacek@ivolution.pl>
2018-03-12 18:44:17 +0100

    installed expressjs + created first API call to list all courses

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 var findAllCourses_1 = require("./queries/findAllCourses");
-findAllCourses_1.findAllCourses().then(function (results) { return console.log(JSON.stringify(results)); });
+var express_1 = __importDefault(require("express"));
+// defining express application
+var app = express_1.default();
+// defining routes
+app.route("/api/courses").get(function (req, resp) {
+    findAllCourses_1.findAllCourses().then(function (results) {
+        resp.status(200).json({ results: results });
+    });
+});
+app.listen(8090, function () {
+    console.log("Server is running");
+});
 //# sourceMappingURL=server.js.map
--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,18 @@
 import { findAllCourses } from "./queries/findAllCourses";
 
-findAllCourses().then(results => console.log(JSON.stringify(results)));
+import express from "express";
+import { Application } from "express-serve-static-core";
+
+// defining express application
+const app:Application = express();
+
+// defining routes
+app.route("/api/courses").get((req, resp) => {
+  findAllCourses().then(results => {
+    resp.status(200).json({results});
+  });
+});
+
+app.listen(8090, () => {
+  console.log("Server is running");
+});
