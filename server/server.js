d62442511b28bbb0fac0ecbbbe269ef45fafea8b
Jacek <jacek@ivolution.pl>
2018-03-12 17:27:12 +0100

    first sql querries

--- server/server.js
+++ server/server.js
@@ -1,3 +1,24 @@
 "use strict";
-console.log("Server is running");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sequelize_1 = __importDefault(require("Sequelize"));
+// import {Sequelize} from "sequelize";
+var dbURL = "postgres://postgres:postgres@localhost:"
+    + "5432/complete-typescript-course";
+var sequelize = new Sequelize_1.default(dbURL);
+var CourseModel = sequelize.define("Course", {
+    description: Sequelize_1.default.STRING,
+    url: Sequelize_1.default.STRING,
+    longDescription: Sequelize_1.default.TEXT,
+    iconUrl: Sequelize_1.default.STRING,
+    courseListIcon: Sequelize_1.default.STRING,
+    seqNo: Sequelize_1.default.INTEGER,
+    comingSoon: Sequelize_1.default.BOOLEAN,
+    isNew: Sequelize_1.default.BOOLEAN,
+    isOngoing: Sequelize_1.default.BOOLEAN,
+});
+var result = CourseModel.findAll()
+    .then(function (results) { return console.log(JSON.stringify(results)); });
 //# sourceMappingURL=server.js.map
--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,21 @@
-console.log("Server is running");
+import ORM from "Sequelize";
+// import {Sequelize} from "sequelize";
+
+const dbURL = "postgres://postgres:postgres@localhost:"
+  + "5432/complete-typescript-course";
+
+const sequelize = new ORM(dbURL);
+
+const CourseModel = sequelize.define("Course", {
+  description:ORM.STRING,
+  url: ORM.STRING,
+  longDescription: ORM.TEXT,
+  iconUrl: ORM.STRING,
+  courseListIcon: ORM.STRING,
+  seqNo: ORM.INTEGER,
+  comingSoon: ORM.BOOLEAN,
+  isNew: ORM.BOOLEAN,
+  isOngoing: ORM.BOOLEAN,
+});
+const result = CourseModel.findAll()
+  .then(results => console.log(JSON.stringify(results)));
