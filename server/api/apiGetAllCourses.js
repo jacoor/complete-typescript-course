d8e49d92cebe48c168e7c10ab38978b14e042c1f
Jacek <jacek@ivolution.pl>
2018-03-13 14:36:51 +0100

    added onSuccess handler in it's own file.

--- server/api/apiGetAllCourses.js
+++ server/api/apiGetAllCourses.js
@@ -10,16 +10,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var findAllCourses_1 = require("../queries/findAllCourses");
 var _ = __importStar(require("lodash"));
 var onError_1 = require("./onError");
+var onSuccess_1 = require("./onSuccess");
 function apiGetAllCourses(req, res) {
     findAllCourses_1.findAllCourses()
-        .then(function () {
-        // error thrown here, inside promise chain,
-        // will not be caught by middleware
-        throw new Error("Error in promise chain!");
-    })
-        .then(function (results) {
-        res.status(200).json({ results: results });
-    })
+        .then(_.partial(onSuccess_1.onSuccess, res))
         .catch(_.partial(onError_1.onError, res, "findAllCourses failed"));
 }
 exports.apiGetAllCourses = apiGetAllCourses;
--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -4,17 +4,20 @@ import { Request, Response } from "express";
 
 import * as _ from "lodash";
 import { onError } from "./onError";
+import { onSuccess } from "./onSuccess";
 
 export function apiGetAllCourses (req:Request, res:Response ){
   findAllCourses()
-  .then(()=>{
+  // .then(()=>{
     // error thrown here, inside promise chain,
     // will not be caught by middleware
-    throw new Error("Error in promise chain!");
-  })
-  .then(results => {
-    res.status(200).json({results});
-  })
+    // this error is being catch by .catch function at the end of this
+    // response handling chain.
+    // throw new Error("Error in promise chain!");
+    // left for learning purposes.
+  // })
+  // refactoring of default success
+  .then(_.partial(onSuccess, res))
   // a way to catch errors in response chain
   // version 1, pretty ugly and verbose, not preferred
   //.catch(err => onError(res, "findAllCourses", err));
