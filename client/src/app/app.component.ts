01c09cff2eacdc124594190b05d1564af3343a78
Jacek <jacek@ivolution.pl>
2018-03-22 09:00:44 +0100

    client app.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from "./services/courses.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
+  constructor (private CoursesService:CoursesService){
+
+  }
+
+  ngOnInit(){
+    this.CoursesService.loadCourseDetail(1)
+      .subscribe(console.log);
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { CoursesService } from "./services/courses.service";
+
+import "rxjs/add/operator/map";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
